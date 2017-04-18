1df16709498a88c51ffb2340d7d1752f083ababe
coder-neo <coder.neonr@gmail.com>
2017-04-17 23:55:45 -0400

    Updated manifest for iTrust deployment

--- site.pp
+++ site.pp
@@ -1,33 +1,37 @@
 node 'agent2.example.com' {
 # Test message
   notify { "Including packages ${hostname} node.": }
-
-  class {'packages':
-   package_list => ['git','nginx']
+  $package_list = ['git','nginx']
+  package {$package_list:
+   ensure => present,
   }
-
+  include java
 }
 
 node 'agent1.example.com'{
 
-notify{"Including packages module on ${hostname}":}
+notify{"Deploying iTrust on ${hostname}":}
 
 # JAVA INSTALLATION 
 
-include java
+class {'java':
+}
 
 # TOMCAT INSTALLATION 
 tomcat::install{'/opt/tomcat':
-  source_url => 'http://mirror.symnds.com/software/Apache/tomcat/tomcat-9/v9.0.0.M18/bin/apache-tomcat-9.0.0.M18.tar.gz',
+  source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-9/v9.0.0.M19/bin/apache-tomcat-9.0.0.M19.tar.gz',
+  require => Class['java'],
 }
 
 tomcat::instance { 'default' :
  catalina_home => '/opt/tomcat',
+ before => Exec['move-iTrust.war']
 }
 
 # Installing dependencies
 package { ['mysql-server','maven']:
     ensure => present,
+    before => Exec['run-iTrust']
   }
 
 # Using service resource to restart mysql service
@@ -37,28 +41,34 @@ service { 'mysql':
     require => Package['mysql-server'],
   }
 
+package { 'git' :
+  ensure => present,
+}
 # Using vcsrepo module to clone iTrust repo
 vcsrepo { '/home/vagrant/iTrust':
   ensure   => present,
   provider => git,
   source   => 'https://github.com/smsejwan/iTrust.git',
+  before => Exec['run-iTrust'],
+  require => Package['git'],
 }
 
 file { '/etc/mysql/my.cnf':
 ensure => present,
+require => Package['mysql-server'],
 }
-
 # Updating config file and notifying mysql service.
 file_line { 'Update my.cnf':
   ensure => present,
   path   => '/etc/mysql/my.cnf',
   line   => 'lower-case-table-names=1',
   after  => 'skip-external-locking',
+  require => Package['mysql-server'],
   notify => Service['mysql'],
 }
 
 # Executing iTrust
-exec {'Run iTrust':
+exec {'run-iTrust':
 
   cwd  => '/home/vagrant/iTrust',
   path   => '/usr/bin:/usr/sbin:/bin',
@@ -66,19 +76,27 @@ exec {'Run iTrust':
 }
 
 # Move iTrust webapps folder.
-exec {'Move iTrust.war':
+exec {'move-iTrust.war':
 
   cwd  => '/home/vagrant/iTrust/target/',
   path   => '/usr/bin:/usr/sbin:/bin',
-  command =>'mv iTrust-23.0.0.war /opt/tomcat/webapps/iTrust.war'
+  command =>'mv iTrust-23.0.0.war /opt/tomcat/webapps/iTrust.war',
+  require => Exec['run-iTrust'],
 }
+
+#tomcat::war{'iTrust.war':
+#  catalina_base => '/opt/tomcat/',
+#  war_source    => '/opt/tomcat/webapps/iTrust.war',
+#  require => Exec['move-iTrust.war'],
+#}
 
 # Restart tomcat
-exec {'Restart Tomcat':
+#exec {'Restart Tomcat':
 
-  path   => '/usr/bin:/usr/sbin:/bin',
-  command =>'sudo chmod 777 /opt/tomcat/ && /opt/tomcat/bin/startup.sh',
-}
+#  path   => '/usr/bin:/usr/sbin:/bin',
+#  command =>'sudo chmod 777 /opt/tomcat/ && /opt/tomcat/bin/startup.sh',
+#  require => Exec['move-iTrust.war'],
+#}
 
 
 }
@@ -92,20 +110,18 @@ class java{
   }
   exec { "add-apt-repository-oracle":
     command => "/usr/bin/add-apt-repository -y ppa:webupd8team/java",
-    notify  => Exec['apt-get update'],
 }
 
   exec {
     'set-licence-selected':
       command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections';
 
-    'set-licence-seen':
+     'set-licence-seen':
       command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections';
   }
 
   package { 'oracle-java8-installer':
     ensure => "latest",
-    require => [Exec['add-apt-repository-oracle'], Exec['set-licence-selected'], Exec['set-licence-seen']],
+    require => [Exec['add-apt-repository-oracle'], Exec['set-licence-selected'], Exec['set-licence-seen'], Exec['apt-get update']],
   }
 }
-                                                                                                                                                                                          89,1          Bot
